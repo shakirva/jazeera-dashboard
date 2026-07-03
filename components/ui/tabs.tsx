833270ff5e616abeae4a45413860a25bff55b90d
Zahid M K <91457816+Zahidmk@users.noreply.github.com>
2026-07-04 00:10:36 +0530

    [updt]: tabs update

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -66,8 +66,8 @@ const TabsTrigger = React.forwardRef<
       className={cn(
         "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
         isActive
-          ? "bg-background text-foreground shadow-sm"
-          : "text-muted-foreground hover:text-foreground",
+          ? "bg-blue-600 text-white shadow-sm"
+          : "text-slate-500 hover:text-slate-900",
         className
       )}
       {...props}
